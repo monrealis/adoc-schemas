430455dd57a00f0f57b61adc5b33bd0dbcaa308b
Vytenis Bivainis <monrealis@gmail.com>
2017-04-14 00:00:38 +0300

    Reformat command

--- src/main/resources/xsd/generate.sh
+++ src/main/resources/xsd/generate.sh
@@ -2,4 +2,9 @@
 
 rm -rf generated
 mkdir generated
-xjc -no-header -d generated -catalog catalog.xml XAdES.xsd adoc-2014-metadata-unmodifiable.xsd adoc-2014-metadata-modifiable.xsd relations.xsd ASiC.xsd
+xjc -no-header -d generated -catalog catalog.xml \
+	XAdES.xsd \
+	adoc-2014-metadata-unmodifiable.xsd \
+	adoc-2014-metadata-modifiable.xsd \
+	relations.xsd \
+	ASiC.xsd
